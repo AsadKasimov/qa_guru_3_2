d33930565ed4a0bbd08a39ebf189d948b199fbf6
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2022-11-24 22:49:44 +0300

    добавил негативный тест

--- homework.py
+++ homework.py
@@ -5,3 +5,8 @@ from selene import be, have
 def test_serh(open_browser):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
+
+
+def test_serh_negative(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('kduffsvdlnkgushugshndliugerfuisfkjslzdjcnslvj').press_enter()
+    browser.element('[id="result-stats"]').should(have.text('About 0 results'))
